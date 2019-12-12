0e418ebc3a051699db29960462a1242567d0d1f0
Alexis M <iamsnkh@pm.me>
2019-12-12 06:48:23 +0100

    :hammer: fix: good %s!

--- srcs/pf_converter.c
+++ srcs/pf_converter.c
@@ -6,7 +6,7 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/07 04:57:06 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/12 01:52:59 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/12 06:46:09 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,22 +20,6 @@ char		*pf_create_blank_string(int width)
 	return (ft_memset(ft_calloc(width + 1, sizeof(char)), 32, width));
 }
 
-void		pf_apply_minus(t_pfinfo *p, char *str)
-{
-	if (p->flags & PF_PRECIS)
-		pf_add_content(p, ft_strncpy(
-			ft_calloc(p->precision, sizeof(char)), str, p->precision));
-	else
-		pf_add_content(p, str);
-	if (p->flags & PF_WIDTH && p->width > (int)ft_strlen(str))
-		return (pf_add_content(p, pf_create_blank_string(p->width -
-		(int)ft_strlen(str))));
-	else
-		pf_add_content(p, pf_create_blank_string(p->width - ft_strlen(str)
-		+ (ft_strlen(str) - p->precision)));
-	p->flags = 0;
-}
-
 void		pf_convert_string(t_pfinfo *p)
 {
 	char	*str;
@@ -43,24 +27,24 @@ void		pf_convert_string(t_pfinfo *p)
 	str = va_arg(p->va, char *);
 	if (!str)
 		str = "(null)";
-	if (!p->flags || (ft_strlen(str) == (size_t)p->width &&
-	!(p->flags & PF_PRECIS)))
+	str = ft_strncpy(ft_calloc(ft_strlen(str) + 1, sizeof(char)),
+	str, p->flags & PF_PRECIS ? p->precision : ft_strlen(str));
+	if (!p->flags || (ft_strlen(str) == (size_t)p->width))
 		return (pf_add_content(p, str));
 	if (p->flags & PF_MINUS)
-		return (pf_apply_minus(p, str));
-	if (p->flags & PF_WIDTH)
 	{
-		if (p->flags & PF_PRECIS && p->precision < (int)ft_strlen(str))
-			pf_add_content(p, pf_create_blank_string(p->width - ft_strlen(str)
-			+ (ft_strlen(str) - p->precision)));
-		else
-			pf_add_content(p, pf_create_blank_string(
-				ft_strlen(str) - p->width));
-	}
-	if (p->flags & PF_PRECIS)
-		pf_add_content(p, ft_strncpy(ft_calloc(
-			p->precision, sizeof(char)), str, p->precision));
-	else
 		pf_add_content(p, str);
+		if (p->flags & PF_WIDTH && p->width > (int)ft_strlen(str))
+			return (pf_add_content(p, pf_create_blank_string(
+				p->width - (int)ft_strlen(str))));
+		else
+			return ;
+	}
+	if (p->flags & PF_WIDTH && p->width > (int)ft_strlen(str))
+		pf_add_content(p, pf_create_blank_string(p->width -
+		(int)ft_strlen(str)));
+	pf_add_content(p, str);
+	p->width = 0;
+	p->precision = 0;
 	p->flags = 0;
 }

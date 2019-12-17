8de3c6d8adcd8fc71e256db187ef1bc2b87a0fa7
Alexis M <iamsnkh@pm.me>
2019-12-17 17:34:54 +0100

    :hammer: [libft] ref: use ft_numlen instead of get_size

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,28 +6,12 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/23 17:14:13 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/05 14:05:07 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/17 17:29:46 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static	int		ft_get_size(int number)
-{
-	int		count;
-
-	count = 0;
-	if (number < 0)
-		number = -number;
-	while (number > 9)
-	{
-		number = number / 10;
-		count++;
-	}
-	count++;
-	return (count);
-}
 
 char			*ft_itoa(int n)
 {
@@ -35,7 +19,7 @@ char			*ft_itoa(int n)
 	int		neg;
 	char	*tab;
 
-	size = ft_get_size(n);
+	size = ft_numlen(n);
 	neg = 0;
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
--- libft/ft_lltoa_base.c
+++ libft/ft_lltoa_base.c
@@ -6,27 +6,12 @@
 /*   By: amonteli <amonteli@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/05 04:01:50 by amonteli     #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/17 15:23:49 by amonteli    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/17 17:30:15 by amonteli    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static	int		ft_get_size(int number)
-{
-	int		count;
-
-	count = 0;
-	if (number < 0)
-		number = -number;
-	while (number > 9)
-	{
-		number = number / 10;
-		count++;
-	}
-	return (count + 1);
-}
 
 char			*ft_lltoa_base(long long n, char *base)
 {
@@ -34,7 +19,7 @@ char			*ft_lltoa_base(long long n, char *base)
 	int		neg;
 	char	*tab;
 
-	size = ft_get_size(n);
+	size = ft_numlen(n);
 	neg = 0;
 	if (n < 0)
 	{
